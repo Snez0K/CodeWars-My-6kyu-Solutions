dbb53c47883183b3022b767a776956d5d1f96226
Ruslan Butkaliuk <Snez0kUA@yandex.ua>
2021-07-22 20:37:16 +0300

    Create (WIP)Hide-phone-numbers.cs

---
+++ (WIP)Hide-phone-numbers.cs
@@ -0,0 +1,21 @@
+/*You need to hide subscribers' phone numbers and check their prefixes in order to send the encrypted file to a client.
+
+Your tasks are:
+
+to hide the last six digits of each phone number
+to check validity of prefixes
+Example 1
+Original phone number	Encrypted
+1-201-680-0202	1-201-6XX-XXXX
+The valid formats for the Italian and US (the last one) numbers are the following:
+
+CASE 1: +39 <separator> ### <separator> ### <separator> ####
+
+CASE 2: 0039 <separator> ### <separator> ### <separator> ####
+
+CASE 3: 1 <separator> ### <separator> ### <separator> ####
+
+The list of separators is the following:
+
+for Italian numbers: " ", ".", ""
+for US numbers: " ", ".", "", "-"*/
