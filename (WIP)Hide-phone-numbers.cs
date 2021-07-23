61f7a0a165235166e85c2d8551df91d687b206fa
Ruslan Butkaliuk <Snez0kUA@yandex.ua>
2021-07-23 12:33:11 +0300

    Update (WIP)Hide-phone-numbers.cs

--- (WIP)Hide-phone-numbers.cs
+++ (WIP)Hide-phone-numbers.cs
@@ -20,16 +20,31 @@ The list of separators is the following:
 for Italian numbers: " ", ".", ""
 for US numbers: " ", ".", "", "-"*/
 
-string result = "www.codewars.com/katas/?page=1#about";
-            //Regex regex = new Regex(@"\#.+");
-            Regex regex = new Regex(@"^[1][ .-]|^0039[ .-]|^+39[ .-]");
-            Regex back = new Regex(@"[ .-][0-9]{3}[ .-][0-9]{3}[ .-][0-9]{4}");
-            Regex replace = new Regex(@"[0-9]");
 
-            //string boy = regex.Match(result).Success ? back.Match(result).Success ? Regex.Replace(result, replace ) : "DoubleGay" : "Gay";
+///WIP WIP WIP WIP WIP FOR SOLUTION WIP WIP WIP WIP
+static void Main(string[] args)
+        {
+            string result = "+39 328 6587120";
+            Regex regex = new Regex(@"^[1][ .-]|^0039[ .-]|^\+39[ .-]");
+            Regex back = new Regex(@"[ .-][0-9]{3}[ .-][0-9]{3}[ .-][0-9]{4}|[ .-][0-9]{7}");
 
-            string boy = regex.Match(result).Success ? back.Match(result).Success ? result. : "DoubleGay" : "Gay";
-            //string boy = regex.Match(result).Success ? result.Replace(regex.Match(result).Value, "") : "Gay";
+            char[] boy = regex.Match(result).Success ? back.Match(result).Success ? result.ToCharArray() : "SecondFalse".ToCharArray() : "False".ToCharArray(); //Там где X оно реплейсит все
+            int temp = 6;
 
-            //string boy = regex.Match(result).Value;
-            //Console.WriteLine(boy);
+            for (int i = boy.Length - 1; i > 0; i--)
+            {
+                if (temp == 0)
+                {
+                    break;
+                }
+                if (char.IsDigit(boy[i]))
+                {
+                    boy[i] = 'X';
+                    temp--;
+                }
+            }
+
+            for (int b = 0; b < boy.Length; b++)
+            {
+                Console.Write(boy[b]);
+            }
